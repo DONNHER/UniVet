a618b78745b72d2f84715cf789a461fba37846ea
DONNHER <dionherlovec@gmail.com>
2025-03-31 07:55:56 +0800

    Initialize

---
+++ app/src/main/java/com/example/uni/management/AppointmentManager.java
@@ -0,0 +1,11 @@
+package com.example.uni.management;
+
+import com.example.uni.entities.Appointment;
+
+import java.util.ArrayList;
+
+public class AppointmentManager {
+    private ArrayList<Appointment> appointments;
+
+
+}
